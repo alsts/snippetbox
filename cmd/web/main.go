4fa285e13d47bc8d28099d0bcde84d4480a950b4
alexsts <astserba@gmail.com>
2024-10-20 21:45:19 +0100

    Templating

--- cmd/web/main.go
+++ cmd/web/main.go
@@ -3,14 +3,36 @@ package main
 import (
 	"log"
 	"net/http"
+	"strings"
 )
 
+type home_struct struct{}
+
+func (h *home_struct) ServeHTTP(w http.ResponseWriter, r *http.Request) {
+	w.Write([]byte("This is my home page"))
+}
+
 func main() {
+	fileServer := http.FileServer(http.Dir("./ui/static/"))
 	mux := http.NewServeMux()
+	mux.Handle("/static/", http.StripPrefix("/static", neuter(fileServer)))
+	mux.Handle("/", &home_struct{})
 	mux.HandleFunc("/", home)
 	mux.HandleFunc("/snippet/view", snippetView)
 	mux.HandleFunc("/snippet/create", snippetCreate)
 	log.Println("Starting server on :4000")
 	err := http.ListenAndServe("127.0.0.1:4000", mux)
 	log.Fatal(err)
+}
+
+// Disable file listing folder structure - only allow direct files retrieval!
+func neuter(next http.Handler) http.Handler {
+	return http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
+		if strings.HasSuffix(r.URL.Path, "/") {
+			http.NotFound(w, r)
+			return
+		}
+
+		next.ServeHTTP(w, r)
+	})
 }
