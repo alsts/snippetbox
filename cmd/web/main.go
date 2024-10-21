399ee6ef64e7943ec2a6d3511c17acf478e38dd9
Aleks Stserbatski <astserba@gmail.com>
2024-10-21 20:58:57 +0100

    Centralised error handling, routes refactor

--- cmd/web/main.go
+++ cmd/web/main.go
@@ -1,38 +1,40 @@
 package main
 
 import (
+	"flag"
 	"log"
 	"net/http"
-	"strings"
+	"os"
+	
 )
 
-type home_struct struct{}
-
-func (h *home_struct) ServeHTTP(w http.ResponseWriter, r *http.Request) {
-	w.Write([]byte("This is my home page"))
-}
+type application struct { 
+    errorLog *log.Logger 
+    infoLog  *log.Logger
+} 
 
 func main() {
-	fileServer := http.FileServer(http.Dir("./ui/static/"))
-	mux := http.NewServeMux()
-	mux.Handle("/static/", http.StripPrefix("/static", neuter(fileServer)))
-	mux.Handle("/", &home_struct{})
-	mux.HandleFunc("/", home)
-	mux.HandleFunc("/snippet/view", snippetView)
-	mux.HandleFunc("/snippet/create", snippetCreate)
-	log.Println("Starting server on :4000")
-	err := http.ListenAndServe("127.0.0.1:4000", mux)
-	log.Fatal(err)
+	var addr *string = flag.String("addr", ":4000", "HTTP network address")
+	flag.Parse()
+
+	infoLog := log.New(os.Stdout, "INFO\t", log.Ldate|log.Ltime)
+	errorLog := log.New(os.Stderr, "ERROR\t", log.Ldate|log.Ltime|log.Lshortfile)
+
+	app := &application {
+		infoLog: infoLog,
+		errorLog: errorLog,
+	}
+
+	srv := &http.Server{
+		Addr:     *addr,
+		ErrorLog: errorLog,
+		Handler:  app.routes(),
+	}
+
+	infoLog.Printf("Starting server on %s", *addr)
+
+	err := srv.ListenAndServe()
+	errorLog.Fatal(err)
 }
 
-// Disable file listing folder structure - only allow direct files retrieval!
-func neuter(next http.Handler) http.Handler {
-	return http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
-		if strings.HasSuffix(r.URL.Path, "/") {
-			http.NotFound(w, r)
-			return
-		}
 
-		next.ServeHTTP(w, r)
-	})
-}
