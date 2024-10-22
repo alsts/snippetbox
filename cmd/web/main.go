de9c3b8aa7cfcda0f41c2867243951f2c0c30e0e
Alex <astserba@gmail.com>
2024-10-22 21:47:56 +0100

    Dynamic HTML templates

--- cmd/web/main.go
+++ cmd/web/main.go
@@ -3,22 +3,24 @@ package main
 import (
 	"database/sql"
 	"flag"
+	_ "github.com/go-sql-driver/mysql" // Import for init() on DB driver
+	"html/template"
 	"log"
 	"net/http"
 	"os"
- 	"snippetbox.alsts.net/internal/models"
-	_ "github.com/go-sql-driver/mysql" // Import for init() on DB driver
+	"snippetbox.alsts.net/internal/models"
 )
 
 type application struct {
-	errorLog *log.Logger
-	infoLog  *log.Logger
-	snippets *models.SnippetModel
+	errorLog      *log.Logger
+	infoLog       *log.Logger
+	snippets      *models.SnippetModel
+	templateCache map[string]*template.Template
 }
 
 func main() {
 	addr := flag.String("addr", ":4000", "HTTP network address")
-	dsn := flag.String("dsn", "web:pass@/snippetbox?parseTime=true", "MySQL data source name")
+	dsn := flag.String("dsn", "root:change-me@/snippetbox?parseTime=true", "MySQL data source name")
 	flag.Parse()
 
 	infoLog := log.New(os.Stdout, "INFO\t", log.Ldate|log.Ltime)
@@ -30,10 +32,17 @@ func main() {
 	}
 	defer db.Close()
 
+	// Initialize a new template cache...
+	templateCache, err := newTemplateCache()
+	if err != nil {
+		errorLog.Fatal(err)
+	}
+
 	app := &application{
 		infoLog:  infoLog,
 		errorLog: errorLog,
 		snippets: &models.SnippetModel{DB: db},
+		templateCache: templateCache,
 	}
 
 	srv := &http.Server{
