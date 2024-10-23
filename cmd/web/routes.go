7f6819e24eb78d07d91755e2ae89656044d2ede2
Alex <astserba@gmail.com>
2024-10-23 21:39:37 +0100

    middleware

--- cmd/web/routes.go
+++ cmd/web/routes.go
@@ -1,12 +1,13 @@
 package main
 
 import (
+	"github.com/justinas/alice"
 	"net/http"
 	"strings"
 )
 
 // The routes() method returns a servemux containing our application routes.
-func (app *application) routes() *http.ServeMux {
+func (app *application) routes() http.Handler {
 	mux := http.NewServeMux()
 
 	fileServer := http.FileServer(http.Dir("./ui/static/"))
@@ -16,7 +17,14 @@ func (app *application) routes() *http.ServeMux {
 	mux.HandleFunc("/snippet/view", app.snippetView)
 	mux.HandleFunc("/snippet/create", app.snippetCreate)
 
-	return mux
+	// return app.recoverPanic(app.logRequest(secureHeaders(mux)))
+
+	// Create a middleware chain containing our 'standard' middleware
+	// which will be used for every request our application receives.
+	standard := alice.New(app.recoverPanic, app.logRequest, secureHeaders)
+
+	// Return the 'standard' middleware chain followed by the servemux.
+	return standard.Then(mux)
 }
 
 // Disable file listing folder structure - only allow direct files retrieval!
