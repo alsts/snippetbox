ad2fd2786079041d99a043ec92b31c129e615934
Alex <astserba@gmail.com>
2024-10-25 19:01:36 +0100

    Session manager

--- cmd/web/main.go
+++ cmd/web/main.go
@@ -3,6 +3,8 @@ package main
 import (
 	"database/sql"
 	"flag"
+	"github.com/alexedwards/scs/mysqlstore"
+	"github.com/alexedwards/scs/v2"
 	"github.com/go-playground/form/v4"
 	_ "github.com/go-sql-driver/mysql" // Import for init() on DB driver
 	"html/template"
@@ -10,14 +12,16 @@ import (
 	"net/http"
 	"os"
 	"snippetbox.alsts.net/internal/models"
+	"time"
 )
 
 type application struct {
-	errorLog      *log.Logger
-	infoLog       *log.Logger
-	snippets      *models.SnippetModel
-	templateCache map[string]*template.Template
-	formDecoder   *form.Decoder
+	errorLog       *log.Logger
+	infoLog        *log.Logger
+	snippets       *models.SnippetModel
+	templateCache  map[string]*template.Template
+	formDecoder    *form.Decoder
+	sessionManager *scs.SessionManager
 }
 
 func main() {
@@ -40,12 +44,18 @@ func main() {
 		errorLog.Fatal(err)
 	}
 
+	// Initialize Session Manager
+	sessionManager := scs.New()
+	sessionManager.Store = mysqlstore.New(db)
+	sessionManager.Lifetime = 12 * time.Hour
+
 	app := &application{
 		infoLog:       infoLog,
 		errorLog:      errorLog,
 		snippets:      &models.SnippetModel{DB: db},
 		templateCache: templateCache,
 		formDecoder:   form.NewDecoder(),
+		sessionManager: sessionManager,
 	}
 
 	srv := &http.Server{
